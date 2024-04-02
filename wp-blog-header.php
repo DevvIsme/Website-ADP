abbb02afd6a1ec9c73be259bd93758ddcac3b298
NgThPhuc <nguyenphuch5p@gmail.com>
2024-04-02 16:09:56 +0700

    load và khởi tạo môi trường của WP

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -6,16 +6,22 @@
  */
 
 if ( ! isset( $wp_did_header ) ) {
+    $wp_did_header = true;
 
-	$wp_did_header = true;
+    // Kiểm tra xem tệp wp-load.php có tồn tại không
+    $wp_load_path = __DIR__ . '/wp-load.php';
+    if ( file_exists( $wp_load_path ) ) {
+        // Nếu có, tiến hành tải thư viện WordPress
+        require_once $wp_load_path;
 
-	// Load the WordPress library.
-	require_once __DIR__ . '/wp-load.php';
+        // Thiết lập truy vấn WordPress
+        wp();
 
-	// Set up the WordPress query.
-	wp();
-
-	// Load the theme template.
-	require_once ABSPATH . WPINC . '/template-loader.php';
-
+        // Tải template của chủ đề
+        require_once ABSPATH . WPINC . '/template-loader.php';
+    } else {
+        // Nếu không tìm thấy tệp wp-load.php, hiển thị thông báo lỗi
+        echo "Lỗi: Không tìm thấy tệp wp-load.php";
+    }
 }
+
